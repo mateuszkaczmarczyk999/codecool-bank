ddf7977c78aab31f3f68335de6b24f3afc30ad37
Cezary Broś <cezary.bros+git@gmail.com>
2017-06-07 19:43:17 +0200

    Fix find and resultSetToAccountType

--- src/main/java/dao/AccountTypeDaoSQLite.java
+++ src/main/java/dao/AccountTypeDaoSQLite.java
@@ -4,7 +4,6 @@ package dao;
 import model.AccountType;
 import util.DatabaseConnection;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,17 +13,21 @@ public class AccountTypeDaoSQLite implements AccountTypeDao {
     DatabaseConnection dbConnect = new DatabaseConnection();
 
     public AccountType find(Integer id) throws SQLException {
-        String query = "SELECT * FROM account_type WHERE accounttypeID = (?)";
+        String query = "SELECT * FROM account_type WHERE accounttypeID = ?";
         PreparedStatement preparedStatement = this.dbConnect.getConnection().prepareStatement(query);
         preparedStatement.setInt(1, id);
         return resultSetToAccountType(preparedStatement.executeQuery());
     }
 
     private AccountType resultSetToAccountType (ResultSet resultSet) throws SQLException {
-        Integer id = resultSet.getInt("accounttypeID");
-        String name = resultSet.getString("name");
-        String description = resultSet.getString("description");
-
+        Integer id = null;
+        String name = null;
+        String description = null;
+        while (resultSet.next()) {
+            id = resultSet.getInt("accounttypeID");
+            name = resultSet.getString("name");
+            description = resultSet.getString("description");
+        }
         return new AccountType(id, name, description);
     }
 
