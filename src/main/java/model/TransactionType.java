a22e2fef59e7f8b82de1b318bdaab422c84f4246
Cezary Broś <cezary.bros+git@gmail.com>
2017-06-07 19:43:55 +0200

    Adjust to Abstracttype demands

--- src/main/java/model/TransactionType.java
+++ src/main/java/model/TransactionType.java
@@ -2,4 +2,11 @@ package model;
 
 
 public class TransactionType extends AbstractType {
+    TransactionType(String name, String description) {
+        super(name, description);
+    }
+
+    TransactionType(Integer id, String name, String description) {
+        super(id, name, description);
+    }
 }
